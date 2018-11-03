08a0c900d31175e3b2ffddd02365f9edd26f66d4
Mihailo Stevanović <mihailo.stev@gmail.com>
2018-11-03 14:32:09 +0100

    Great new user and alert others

--- server/server.js
+++ server/server.js
@@ -12,7 +12,19 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected.');   
+    console.log('New user connected.'); 
+    
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
@@ -20,7 +32,7 @@ io.on('connection', (socket) => {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        });
+        });        
     });
 
     socket.on('disconnect', () => {
