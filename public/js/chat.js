3c459691562f25dff82d851c289a4e3235c57f55
Mihailo Stevanović <mihailo.stev@gmail.com>
2018-11-09 13:42:11 +0100

    Send messages only to in the same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -72,8 +72,7 @@ jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
     var messageTextBox = jQuery('[name=message]');
-    socket.emit('createMessage', {
-        from: 'User',
+    socket.emit('createMessage', {        
         text: messageTextBox.val()
     }, function(){
         messageTextBox.val('');
